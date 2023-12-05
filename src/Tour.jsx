8597c5991bdedee7cf421ff781059632d025ab03
esma <esma.ea.aksoy@gmail.com>
2023-12-05 15:09:46 +0300

    remove

---
+++ src/Tour.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+const Tour = ({ id, name, image, info, price }) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <div className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={()=> removeTour(id)}>not interested</button>
+      </footer>
+    </div>
+  );
+};
+
+export default Tour;
---
+++ src/Tours.jsx
@@ -0,0 +1,18 @@
+import Tour from "./Tour";
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => (
+          <Tour key={tour.id} {...tour} removeTour={removeTour}/>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
